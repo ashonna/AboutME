25a11b6e6725f30f434cc5640cb7cc8c4f8c533e
Aditi Honna <aditihonna@gmail.com>
2023-06-23 15:02:40 -0400

    Initial Commit

---
+++ AboutME/AboutMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMEApp.swift
+//  AboutME
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
