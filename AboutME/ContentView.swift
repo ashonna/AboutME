a2e4811086716e7eefee5cca2200dee3b741f670
Aditi Honna <aditihonna@gmail.com>
2023-06-26 15:38:54 -0400

    uploading

--- AboutME/ContentView.swift
+++ AboutME/ContentView.swift
@@ -2,20 +2,116 @@
 //  ContentView.swift
 //  AboutME
 //
-//  Created by Scholar on 6/23/23.
+//  Created by Aditi  Honna on 6/23/23.
+//
+
+import SwiftUI
+
+//
+//  ContentView.swift
+//  AboutME
+//
+//  Created by Aditi  Honna on 6/23/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false // Move the declaration here
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("Hi there! I'm Aditi. Welcome to my chaotic world!")
+                    .font(.headline)
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+                Image("FIRST")
+                    .resizable()
+                    .padding(15.0)
+                    .frame(width: 350, height: 431.0)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                
+                Text("Proceed with CAUTION!")
+                    .multilineTextAlignment(.center)
+                Button("CAUTION") {
+                    self.showDetails = true
+                    
+                }
+                .buttonStyle(OutlineButtonStyle())
+                
+                    VStack {
+                        Text("Here's more context...")
+                            .font(.title)
+                            .padding()
+                        
+                        VStack {
+                            HStack {
+                                Image("loveFAM")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding()
+                                
+                                Text("My true best friends right here. I love family; they always stick with me through thick and thin.")
+                            }
+                            .padding()
+                            .background(Color.cyan)
+                            //end of first HSTACK
+                            
+                            HStack {
+                                Image("Nature")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding()
+                                    
+                                Text("I love the outdoors despite the bugs and the scary stuff but sometimes i get really tired and started grumbling about regretting the trip to my fam.")
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            //end of second HSTACK
+                            
+                            HStack {
+                                Image("me&brother")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding()
+                                
+                                Text("I have a brother who said he didn't want to be on this app but i put him on anyway cause i love him.")
+                            }
+                            .padding()
+                            .background(Color.cyan)
+                            //end of third HSTACK
+                            
+                            HStack {
+                                Image("dog")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding()
+                                
+                                Text("I LOVE DOGS!!I really want a golden retiever so I'm trying convice my parents which has been going on since i was 9. Good luck to me.")
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            //end of fourth HSTACK
+                        }
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+
+
+struct OutlineButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 2)
+                    .background(Color.cyan)
+            )
     }
 }
 
